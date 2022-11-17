7f1998831d1bfc3e6f44ec34ae4f45a038ec977f
rahul.karande <rahul.karande@bnt-soft.com>
2022-11-17 14:22:34 +0530

    inital commit

---
+++ app/src/main/java/com/vastu/realestate/rootModule/splash/view/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.vastu.realestate.rootModule.splash.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vastu.realestate.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
