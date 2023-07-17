5e1d0809db9f493701fe4ce4b5a75b16da8906de
Pratik <pratikgondil@gmail.com>
2023-07-17 22:42:56 +0530

    added the added the contact us

---
+++ app/src/main/java/com/vastu/realestate/appModule/selectlanguage/SelectLanguageFragment.kt
@@ -0,0 +1,42 @@
+  package com.vastu.realestate.appModule.selectlanguage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.vastu.realestate.R
+import com.vastu.realestate.appModule.contactus.ContactUsViewModel
+import com.vastu.realestate.appModule.dashboard.view.BaseFragment
+import com.vastu.realestate.appModule.dashboard.viewmodel.DrawerViewModel
+import com.vastu.realestate.databinding.FragmentSelectLanguageBinding
+
+
+  class SelectLanguageFragment : BaseFragment() {
+
+    lateinit var selectLanguageViewModel: SelectLanguageViewModel
+    lateinit var selectLanguageBinding: FragmentSelectLanguageBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        selectLanguageViewModel = ViewModelProvider(this)[SelectLanguageViewModel::class.java]
+        selectLanguageBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_select_language,container,false)
+        selectLanguageBinding.selectLanguageViewModel = selectLanguageViewModel
+        initView()
+        return selectLanguageBinding.root
+    }
+
+      private fun initView() {
+
+      }
+
+
+  }
---
+++ app/src/main/java/com/vastu/realestate/appModule/selectlanguage/SelectLanguageViewModel.kt
@@ -0,0 +1,24 @@
+package com.vastu.realestate.appModule.selectlanguage
+
+import android.app.Application
+import android.app.backup.SharedPreferencesBackupHelper
+import android.content.SharedPreferences
+import androidx.databinding.ObservableField
+import androidx.lifecycle.AndroidViewModel
+import com.vastu.realestate.R
+
+class SelectLanguageViewModel(application: Application): AndroidViewModel(application) {
+    var mContext :Application
+
+    init {
+        mContext = application
+    }
+    var btnBackground = ObservableField(mContext.getDrawable(R.drawable.button_inactive_background))
+
+
+    fun onNextClick()
+    {
+
+    }
+
+}
